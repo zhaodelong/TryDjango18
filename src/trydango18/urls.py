c3de128b699b5f877d8d9e9ca09f2d1fad3fbc45
Delong Zhao <zhaodelong@gmail.com>
2015-10-17 23:20:23 -0500

    finish lecture 6

--- src/trydango18/urls.py
+++ src/trydango18/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'trydango18.views.home', name='home'),
+    url(r'^$', 'newsletter.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
